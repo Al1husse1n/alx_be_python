9fc198437457efe4fc8a6c26e559e2b106683915
Ali Hussein <ah0979075074@gmail.com>
2024-09-24 01:34:25 +0300

    edited

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,14 +3,14 @@ def perform_operation(num1, num2, operation):
         case "add":
             print(num1 + num2)
         case "subtract":
-            print(num2 + num1)
+            print(num1 - num1)
         case "multiply":
             print(num1 * num2)
         case "divide":
             if num1 == 0:
                 print("You can't divide a number by zero")
             else:
-                print(num2 / num1)
+                print(num1 / num2)
 
 
 
