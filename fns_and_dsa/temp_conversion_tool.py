f1771dd6746d9a212195f4e7791e9211ea4776c9
Ali Hussein <ah0979075074@gmail.com>
2024-09-24 16:49:35 +0300

    edited

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,19 +6,19 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 def main():
-    temp_to_convert = input("Enter the temperature to convert: ")
-    temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-    if not temp_to_convert.replace('.', '', 1).isdigit():
-        print("Invalid temperature. Please enter a numeric value.")
-    else:    
+    try:
+        temp_to_convert = float(input("Enter the temperature to convert: "))
+        temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")   
         match temp_type:
             case 'C':
-                fahr = convert_to_fahrenheit(float(temp_to_convert))
-                print(f'{float(temp_to_convert)}ºc is {fahr}ºf')
+                fahr = convert_to_fahrenheit(temp_to_convert)
+                print(f'{temp_to_convert}ºc is {fahr}ºf')
             case 'F':
-                cels = convert_to_celsius(float(temp_to_convert))  
-                print(f'float({temp_to_convert})ºf is {cels}ºc')
+                cels = convert_to_celsius(temp_to_convert)  
+                print(f'{temp_to_convert}ºf is {cels}ºc')
             case _:
                 print("invalid Temperature type")
+    except ValueError :
+        print("Invalid temperature. Please enter a numeric value.")                
 if __name__ == "__main__":
     main()                      
