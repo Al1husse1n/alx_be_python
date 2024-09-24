b9a8e1354e1aa3421b3a1c4e596ab588cb116b8b
Ali Hussein <ah0979075074@gmail.com>
2024-09-24 16:42:18 +0300

    edited it

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,18 +5,20 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(celsius):
     return (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-
-temp_to_convert = input("Enter the temperature to convert: ")
-temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-if not temp_to_convert.replace('.', '', 1).isdigit():
-    print("Invalid temperature. Please enter a numeric value.")
-else:    
-    match temp_type:
-        case 'C':
-            fahr = convert_to_fahrenheit(float(temp_to_convert))
-            print(f'{float(temp_to_convert)}ºc is {fahr}ºf')
-        case 'F':
-            cels = convert_to_celsius(float(temp_to_convert))  
-            print(f'float({temp_to_convert})ºf is {cels}ºc')
-        case _:
-            print("invalid Temperature type")       
+def main():
+    temp_to_convert = input("Enter the temperature to convert: ")
+    temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+    if not temp_to_convert.replace('.', '', 1).isdigit():
+        print("Invalid temperature. Please enter a numeric value.")
+    else:    
+        match temp_type:
+            case 'C':
+                fahr = convert_to_fahrenheit(float(temp_to_convert))
+                print(f'{float(temp_to_convert)}ºc is {fahr}ºf')
+            case 'F':
+                cels = convert_to_celsius(float(temp_to_convert))  
+                print(f'float({temp_to_convert})ºf is {cels}ºc')
+            case _:
+                print("invalid Temperature type")
+if __name__ == "__main__":
+    main()                      
