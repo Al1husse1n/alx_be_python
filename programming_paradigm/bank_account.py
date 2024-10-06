813053a2a16b870c3be66ac04d5501ce8fceab62
Ali Hussein <ah0979075074@gmail.com>
2024-10-06 21:32:20 +0300

    edited

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,4 +10,4 @@ class BankAccount:
         else :
             return False
     def display_balance(self):
-        return f'Current Balance: ${self.account_balance}'             
+        print(f'Current Balance: ${self.account_balance}')             
