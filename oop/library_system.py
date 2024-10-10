2888e87e7ae62918e45b776ae4f6b236e4f470c1
Ali Hussein <ah0979075074@gmail.com>
2024-10-10 22:12:53 +0300

    added a library system

---
+++ oop/library_system.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return f"THe book title is {self.title}, authored by {self.author}"   
+class EBook(Book):
+    def __init__(self, title, author,file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+    def __str__(self):
+        return f"THe book title is {self.title}, authored by {self.author} and it's file size is {self.file_size}"
+class PrintBook(Book):
+    def __init__(self, title, author,page_count):
+        super().__init__(title, author)    
+        self.page_count = page_count
+    def __str__(self):
+        return f"THe book title is {self.title}, authored by {self.author} and it's page count is {self.page_count}"    
+class Library:
+    def __init__(self,books=[]):
+        self.books = books
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for i in self.books:
+            print(i)
+                    
